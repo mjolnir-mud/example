54529df3737f8c3eca89af097dcb260e7ef1b413
Jarod <therealfugu@gmail.com>
2022-07-09 13:42:50 -0600

    clean up engine packaging

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/mjolnir-mud/engine/pkg/engine"
+	"github.com/mjolnir-mud/engine/pkg/plugin"
 	"github.com/mjolnir-mud/plugins/compass"
 	"github.com/mjolnir-mud/plugins/telnet_portal"
 	"github.com/mjolnir-mud/plugins/world"
 )
 
 func main() {
-	engine.Init("example", []engine.Plugin{
+	engine.Init("example", []plugin.Plugin{
 		world.Plugin,
 		telnet_portal.Plugin,
 		compass.Plugin,
